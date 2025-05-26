9ad99b0ffe829838eed46b96d5e9b19d58db92c7
brandon10118 <202310118@fit.edu.ph>
2025-05-26 10:08:25 +0800

    fix(navbar): update links and titles for consistency

--- page/zodiacSigns/index.php
+++ page/zodiacSigns/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Zodiac Sign</title>
-    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="/page/zodiacSigns/assets/css/style.css">
 </head>
 <body>
     <header>
@@ -18,5 +18,5 @@
 
 
 </body>
-<script src="assets/calculations.js"></script>
+<script src="/page/zodiacSigns/assets/js/calculations.js"></script>
 </html>
